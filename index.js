da18dc0f9cc82e4b8043f5b04ca763e81f67dca1
Elisabeth Garcia <lisgarciae@gmail.com>
2023-05-01 16:44:49 -0400

    completed

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var customerName = 'bob'
 function upperCaseCustomerName() {
     customerName=customerName.toUpperCase()
 }
-  
+
 function setBestCustomer() {
    bestCustomer='not bob'
     return bestCustomer
@@ -19,14 +19,3 @@ const leastFavoriteCustomer = 'probably bob'
 function changeLeastFavoriteCustomer() {
     leastFavoriteCustomer='definitely bob'
 }
-
-
-
-//attempt1  return customerName.toUpperCase()  <--this passes the "returns the customerName" requirement
-
-//attempt2a    customerName = customerName.toUpperCase()
-//attempt2b    return customerName
-
-//attempt3a    var upper = customerName.toUpperCase()
-//attempt3b    customerName = upper    
-//attempt3c    return customerName
